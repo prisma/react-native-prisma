db7cf560adbbe4d9d4cc85cadbd4263c86800a32
Oscar Franco <ospfranco@gmail.com>
2024-03-04 07:58:14 +0100

    Remove reanimated

--- src/NativePrisma.ts
+++
@@ -1,8 +0,0 @@
-import type { TurboModule } from 'react-native';
-import { TurboModuleRegistry } from 'react-native';
-
-export interface Spec extends TurboModule {
-  install(): void;
-}
-
-export default TurboModuleRegistry.getEnforcing<Spec>('Prisma');
