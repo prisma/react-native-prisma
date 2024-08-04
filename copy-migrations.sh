c8453176347bec8855653252837155dab0a1f5d0
jennmueng <ldrwarlock@gmail.com>
2024-08-04 13:36:04 -0700

    use prisma/migrations path for migrations script

--- copy-migrations.sh
+++ copy-migrations.sh
@@ -6,6 +6,6 @@ MIGRATIONS_TARGET=${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH
 
 rm -rf "$MIGRATIONS_TARGET/migrations"
 mkdir "$MIGRATIONS_TARGET/migrations"
-cp -r ${SRCROOT}/../migrations ${MIGRATIONS_TARGET}
+cp -r ${SRCROOT}/../prisma/migrations ${MIGRATIONS_TARGET}
 
 echo "migration files copied ✅"
